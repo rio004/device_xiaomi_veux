2d18739aba22c4925c64ae57b487fec9fcea724b
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2023-05-21 00:16:09 +0000

    veux: Build camera libs
    Change-Id: I9b5d730c54018614c8e28a946a1d234663339b0c

--- device.mk
+++ device.mk
@@ -80,6 +80,17 @@ PRODUCT_PACKAGES += \
     vendor.qti.hardware.btconfigstore@1.0.vendor \
     vendor.qti.hardware.btconfigstore@2.0.vendor
 
+# Camera
+PRODUCT_PACKAGES += \
+    android.hardware.camera.provider@2.4-impl \
+    android.hardware.camera.provider@2.4-service_64
+
+PRODUCT_PACKAGES += \
+    libcamera2ndk_vendor \
+    libdng_sdk.vendor \
+    libgui_vendor \
+    libstdc++.vendor
+
 # Partitons
 PRODUCT_BUILD_SUPER_PARTITION := false
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
