478c9eb7c1931ac0cc8ef40cd4209475587c06f8
Volodymyr Zhdanov <wight554@gmail.com>
2023-05-21 02:03:27 +0000

    veux: Build sensors HAL from source
    Change-Id: I6e90d8c80458de8a950818226babc6e8677b0ffb

--- device.mk
+++ device.mk
@@ -247,6 +247,11 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/rootdir/etc/fstab.qcom:$(TARGET_COPY_OUT_VENDOR_RAMDISK)/first_stage_ramdisk/fstab.qcom \
     $(LOCAL_PATH)/rootdir/etc/init.recovery.qcom.rc:$(TARGET_COPY_OUT_RECOVERY)/root/init.recovery.qcom.rc
 
+# Sensors
+PRODUCT_PACKAGES += \
+    android.hardware.sensors@2.0-service.multihal \
+    libsensorndkbridge
+
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH)
