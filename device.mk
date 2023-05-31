0cabad8a3695b3c9dec822908cc284c44af110f7
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2023-05-31 00:30:17 +0000

    veux: Import CarrierConfig from CAF
    * From LA.UM.9.16.r1-13100-MANNAR.QSSI13.0
    
    Change-Id: Iac0ff2be55bb3b8a942e450010bf90776a6c5733

--- device.mk
+++ device.mk
@@ -170,6 +170,7 @@ PRODUCT_PACKAGES += \
 PRODUCT_ENFORCE_RRO_TARGETS := *
 
 PRODUCT_PACKAGES += \
+    CarrierConfigOverlayVeux \
     FrameworksResOverlayVeux \
     SettingsOverlayVeux \
     SystemUIOverlayVeux \
