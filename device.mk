0f1e3a541bba76040f6203fbd453ef5e7723dae4
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2023-05-31 12:59:07 +0000

    veux: Build common Xiaomi light AIDL

--- device.mk
+++ device.mk
@@ -172,6 +172,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.keymaster@4.1.vendor
 
+# Lights
+PRODUCT_PACKAGES += \
+    android.hardware.light-service.xiaomi
+
 # Media
 PRODUCT_PACKAGES += \
     libavservices_minijail \
