d1c8ba9449ff39be060469e39f93733e21f74860
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2023-05-31 13:00:31 +0000

    veux: Build common Xiaomi fingerprint HIDL

--- device.mk
+++ device.mk
@@ -131,6 +131,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     fastbootd
 
+# Fingerprint
+PRODUCT_PACKAGES += \
+    android.hardware.biometrics.fingerprint@2.3-service.xiaomi
+
 # FM
 PRODUCT_PACKAGES += \
     FM2 \
